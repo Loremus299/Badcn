228b1f443f7458b9f7ed221a1a8a597575fb9fe6
loremus <loremus@proton.me>
2026-09-04 19:40:04 +0530

    nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Showcase from "./_components/showcase";
 
 export default function Page() {
@@ -5,8 +6,9 @@ export default function Page() {
     <div className="flex w-full justify-center">
       <main className="w-full max-w-7xl landscape:p-8 portrait:scale-95">
         <div className="h-[75vh] grid place-items-center">
+          <div className="self-start justify-self-start flex gap-4">Badcn</div>
           <div className="grid gap-3">
-            <h1 className="text-center text-6xl tracking-tight font-semibold animate-space-in">
+            <h1 className="text-center text-7xl tracking-tight font-semibold animate-space-in">
               Badcn: shadcn++
             </h1>
             <h2 className="text-center text-muted-foreground leading-5">
@@ -16,6 +18,15 @@ export default function Page() {
           </div>
         </div>
         <Showcase />
+        <div className="mt-8 text-center text-muted-foreground tracking-tight">
+          <Link href={"https://web.loremus.gay"} className="underline">
+            Loremus 2026
+          </Link>{" "}
+          under{" "}
+          <Link href={"https://gal.gay"} className="underline">
+            Gay Agenda License
+          </Link>
+        </div>
       </main>
     </div>
   );
