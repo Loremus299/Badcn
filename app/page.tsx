8b2de3a5c0adfd5c636882a4fc8a3bd15718e51c
loremus <loremus@proton.me>
2026-09-04 18:45:03 +0530

    landing page responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Showcase from "./_components/showcase";
 
 export default function Page() {
   return (
-    <div className="grid justify-center">
-      <main className="w-7xl p-4">
+    <div className="flex w-full justify-center">
+      <main className="w-full max-w-7xl p-4">
+        <h1 className="text-center text-4xl tracking-tight font-semibold">
+          Badcn
+        </h1>
         <Showcase />
       </main>
     </div>
