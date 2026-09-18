495848a5d0bb3ed9c774c5336260b6beb588301a
loremus <loremus@proton.me>
2026-09-19 00:12:52 +0530

    demo fix

--- components/badcn/showcase/colorPicker.tsx
+++ components/badcn/showcase/colorPicker.tsx
@@ -3,7 +3,7 @@ import ColorPicker from "../colorPicker";
 
 export default function ColorPickerDemo() {
   return (
-    <div className="w-full max-w-md p-16 pl-8 pr-8">
+    <div className="w-full max-w-sm p-16 pl-8 pr-8">
       <ColorPicker
         onPick={(e) => {
           toast.add({ title: `Nice color ${e}` });
