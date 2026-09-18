187ef6905fc878ab1c3db4d7c0625c65263237d6
loremus <loremus@proton.me>
2026-09-18 23:14:22 +0530

    YAY

--- components/badcn/colorPicker.tsx
+++ components/badcn/colorPicker.tsx
@@ -80,7 +80,7 @@ export default function ColorPicker({
             onChange={(e) => {
               setColor(e.currentTarget.value);
             }}
-            size={alpha ? 9 : 7}
+            size={color.length - 3}
           />
         </div>
         <Dialog>
